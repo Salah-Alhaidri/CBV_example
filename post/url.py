fb18a2dbb8fe05dd9be2c68747ee837495a9ae24
salah <salah.alhaidri@gmail.com>
2024-12-14 17:37:11 +0300

    add the CBV

--- post/url.py
+++ post/url.py
@@ -3,10 +3,12 @@ from django.contrib import admin
 from django.urls import path,include
 from django.conf import settings
 from django.conf.urls.static import static
+from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('',include('post.url'),),
+    
+    path('',views.post_list.as_view()),
+    # path('<int:pk>',views.post_detail),
 ] 
 # static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 # static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- post/views.py
+++ post/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from .models import Post
+from django.views.generic import ListView,DetailView,CreateView,DeleteView
 
-# Create your views here.
+class post_list(ListView):
+    model=Post
+    template_name='post_list.html'
+    
+
+class postdetails(DetailView):
+    model=Post
+    template_name='post_list.html'
