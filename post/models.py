c4fecda693c618ba8c5456c4218e8a19cc8f47d1
salah <salah.alhaidri@gmail.com>
2024-12-14 16:40:04 +0300

    add post app

--- post/models.py
+++ post/models.py
@@ -1,3 +1,20 @@
 from django.db import models
-
+from django.utils import timezone
 # Create your models here.
+class Post(models.Model):
+    titile=models.CharField(max_length=50)
+    content=models.TextField(max_length=1000)
+    created_at=models.DateField(default=timezone.now)
+    models.ImageField(upload_to='post-img/')
+
+    
+
+    # class Meta:
+        # verbose_name = _("")
+        # verbose_name_plural = _("s")
+
+    def __str__(self):
+        return self.titile
+
+    # def get_absolute_url(self):
+    #     return reverse("_detail", kwargs={"pk": self.pk})
--- project/settings.py
+++ project/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-a1u+(9v0zh=2_bmvhv+ukgh)_z2el7q%d*((mgj+8_va!u*de9'
+SECRET_KEY = 'django-insecure-6f8abs^xnq*3uag(u51uj1u5pb6+-eh!12(rl64m^c9r$4-(__'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'post',
 ]
 
 MIDDLEWARE = [
